f2b6070f80a2d9c9c0f818c3d9df39b929fb0d65
VanekForest <zazc256@gmail.com>
2025-04-20 22:47:19 +0300

    hotfix: исправил неточность с relation ship

--- lawly_db/db_models/document.py
+++ lawly_db/db_models/document.py
@@ -12,4 +12,4 @@ class Document(Base):
     link: Mapped[str] = mapped_column(String, nullable=False)
     description: Mapped[str] = mapped_column(String, nullable=False)
 
-    fields: Mapped[list["DocumentField"]] = relationship("fields", back_populates="document", lazy="selectin")
+    fields: Mapped[list["Field"]] = relationship("Field", back_populates="document", lazy="selectin")
