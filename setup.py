bebe9d9f739d70b88d5b0b02e220935accdfcd19
gagik <gagikgrigoryan2005@mail.ru>
2025-04-21 21:45:32 +0300

    fix: version fix in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lawly_db",
-    version="0.1.0",
+    version="0.1.4",
     author="VanekForest",
     author_email="zazc256@gmail.com",
     description="База данных для сайта Lawly",
