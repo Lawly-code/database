4665687219320231cc0f4e3300e4400fda1ddbaf
VanekForest <zazc256@gmail.com>
2025-04-26 14:10:24 +0300

    fix: добавил авто удаление в relationship

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lawly_db",
-    version="0.2.2",
+    version="0.2.3",
     author="VanekForest",
     author_email="zazc256@gmail.com",
     description="База данных для сайта Lawly",
