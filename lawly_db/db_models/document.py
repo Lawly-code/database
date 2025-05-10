29a26f69dfebf5f0b52dc7b0d6377f7f13154356
VanekForest <zazc256@gmail.com>
2025-05-10 16:00:19 +0300

    refactor: добавил поле is_personal

--- lawly_db/db_models/document.py
+++ lawly_db/db_models/document.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger, String
+from sqlalchemy import BigInteger, String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .db_session import Base
@@ -8,6 +8,7 @@ class Document(Base):
     __tablename__ = "documents"
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_personal: Mapped[bool] = mapped_column(Boolean, server_default='false')
     name_ru: Mapped[str] = mapped_column(String(255), nullable=False)
     link: Mapped[str] = mapped_column(String, nullable=False)
     description: Mapped[str] = mapped_column(String, nullable=False)
